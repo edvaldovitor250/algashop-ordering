f0a5aa57c903ce59d012bd76ac6790d8d8e91a79
Edvaldo Vitor <edvaldovitor250@gmail.com>
2025-07-17 20:39:33 -0300

    Assembler: Domain Entity to Jakarta Persistence Entity Converter

--- src/main/java/com/algaworks/algashop/ordering/domain/repository/Orders.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/repository/Orders.java
@@ -1,8 +1,9 @@
 package com.algaworks.algashop.ordering.domain.repository;
 
 import com.algaworks.algashop.ordering.domain.entity.Order;
-import com.algaworks.algashop.ordering.domain.repository.Repository;
 import com.algaworks.algashop.ordering.domain.valueobject.id.OrderId;
 
 public interface Orders extends Repository<Order, OrderId> {
+
+
 }
