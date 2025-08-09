f6e11cc49f7ca7a4eac45a7df16c9d29e1f661bc
Edvaldo Vitor <edvaldovitor250@gmail.com>
2025-08-09 18:25:58 -0300

    Desafio - Implemente o Domain Service para ShoppingCart

---
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/service/ShoppingService.java
@@ -0,0 +1,13 @@
+package com.algaworks.algashop.ordering.domain.model.service;
+
+import com.algaworks.algashop.ordering.domain.model.entity.ShoppingCart;
+import com.algaworks.algashop.ordering.domain.model.utility.DomainService;
+import com.algaworks.algashop.ordering.domain.model.valueobject.id.CustomerId;
+
+@DomainService
+
+public class ShoppingService {
+
+    public ShoppingCart startShopping(CustomerId customerId)
+
+}
