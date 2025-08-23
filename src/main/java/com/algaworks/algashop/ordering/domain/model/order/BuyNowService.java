d79646b258f1d82bea52d5a1f91aa2c7e144d702
Edvaldo Vitor <edvaldovitor250@gmail.com>
2025-08-22 21:08:16 -0300

    Implementing rule for free shipping

--- src/main/java/com/algaworks/algashop/ordering/domain/model/order/BuyNowService.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/order/BuyNowService.java
@@ -1,30 +1,52 @@
 package com.algaworks.algashop.ordering.domain.model.order;
 
 import com.algaworks.algashop.ordering.domain.model.DomainService;
+import com.algaworks.algashop.ordering.domain.model.commons.Money;
+import com.algaworks.algashop.ordering.domain.model.customer.Customer;
+import com.algaworks.algashop.ordering.domain.model.customer.LoyaltyPoints;
 import com.algaworks.algashop.ordering.domain.model.product.Product;
 import com.algaworks.algashop.ordering.domain.model.commons.Quantity;
 import com.algaworks.algashop.ordering.domain.model.customer.CustomerId;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Year;
 
 @DomainService
+@RequiredArgsConstructor
 public class BuyNowService {
 
-	public Order buyNow(Product product,
-						CustomerId customerId,
-						Billing billing,
-						Shipping shipping,
-						Quantity quantity,
-						PaymentMethod paymentMethod) {
-		
-		product.checkOutOfStock();
+    private final Orders orders;
 
-		Order order = Order.draft(customerId);
-		order.changeBilling(billing);
-		order.changeShipping(shipping);
-		order.changePaymentMethod(paymentMethod);
-		order.addItem(product, quantity);
-		order.place();
+    public Order buyNow(Product product,
+                        Customer customer,
+                        Billing billing,
+                        Shipping shipping,
+                        Quantity quantity,
+                        PaymentMethod paymentMethod) {
 
-		return order;
-	}
+        product.checkOutOfStock();
+
+        Order order = Order.draft(customer.id());
+        order.changeBilling(billing);
+        order.changePaymentMethod(paymentMethod);
+        order.addItem(product, quantity);
+
+        if (haveFreeShipping(customer)) {
+            Shipping freeShipping = shipping.toBuilder().cost(Money.ZERO).build();
+            order.changeShipping(freeShipping);
+        } else {
+            order.changeShipping(shipping);
+        }
+
+        order.place();
+
+        return order;
+    }
+
+    private boolean haveFreeShipping(Customer customer) {
+        return customer.loyaltyPoints().compareTo(new LoyaltyPoints(100)) >= 0
+                && orders.salesQuantityByCustomerInYear(customer.id(), Year.now()) >= 2
+                || customer.loyaltyPoints().compareTo(new LoyaltyPoints(2000)) >= 0;
+    }
 
 }
