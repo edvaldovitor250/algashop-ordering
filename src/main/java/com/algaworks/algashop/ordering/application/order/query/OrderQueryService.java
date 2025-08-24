a1e6ea09fc753e26258fea9087baad4bbb4e03df
Edvaldo Vitor <edvaldovitor250@gmail.com>
2025-08-24 12:30:09 -0300

    Paginated Query for Order with Criteria API

--- src/main/java/com/algaworks/algashop/ordering/application/order/query/OrderQueryService.java
+++ src/main/java/com/algaworks/algashop/ordering/application/order/query/OrderQueryService.java
@@ -1,6 +1,10 @@
 package com.algaworks.algashop.ordering.application.order.query;
 
+import com.algaworks.algashop.ordering.application.utility.PageFilter;
+import org.hibernate.query.Page;
+
 public interface OrderQueryService {
 
     OrderDetailOutput findById(String orderId);
+    Page<OrderSummaryOutput> filter(PageFilter filter);
 }
