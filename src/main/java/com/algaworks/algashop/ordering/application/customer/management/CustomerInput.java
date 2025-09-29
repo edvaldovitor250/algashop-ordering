0232997d11719fdf0bcc974271cbfe3649fc1c91
Edvaldo Vitor <edvaldovitor250@gmail.com>
2025-09-28 21:25:17 -0300

    Location header evaluating

--- src/main/java/com/algaworks/algashop/ordering/application/customer/management/CustomerInput.java
+++ src/main/java/com/algaworks/algashop/ordering/application/customer/management/CustomerInput.java
@@ -1,6 +1,8 @@
 package com.algaworks.algashop.ordering.application.customer.management;
 
 import com.algaworks.algashop.ordering.application.commons.AddressData;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,12 +15,28 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 public class CustomerInput {
+
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String phone;
+
+    @NotBlank
+
     private String document;
+
+    @NotBlank
+    @Past
     private LocalDate birthDate;
+
+    @NotBlank
     private Boolean promotionNotificationsAllowed;
     private AddressData address;
 }
