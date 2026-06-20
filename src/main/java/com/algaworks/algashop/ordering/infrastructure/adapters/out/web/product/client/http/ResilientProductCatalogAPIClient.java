fe5dc543e1d4ae73d7f6a17e5905236793f10c83
Edvaldo Vitor <edvaldovitor250@gmail.com>
2026-06-20 08:36:43 -0300

    Resiliência com Spring Cloud Circuit Breaker

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/web/product/client/http/ResilientProductCatalogAPIClient.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/web/product/client/http/ResilientProductCatalogAPIClient.java
@@ -1,18 +1,19 @@
 package com.algaworks.algashop.ordering.infrastructure.adapters.out.web.product.client.http;
 
-import com.algaworks.algashop.ordering.core.domain.model.commons.Money;
-import com.algaworks.algashop.ordering.core.domain.model.product.Product;
-import com.algaworks.algashop.ordering.core.domain.model.product.ProductId;
-import com.algaworks.algashop.ordering.core.domain.model.product.ProductName;
 import com.algaworks.algashop.ordering.infrastructure.adapters.in.web.exceptionhandler.BadGatewayException;
 import com.algaworks.algashop.ordering.infrastructure.adapters.in.web.exceptionhandler.GatewayTimeoutException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.NoFallbackAvailableException;
+import org.springframework.core.retry.RetryException;
 import org.springframework.resilience.annotation.ConcurrencyLimit;
 import org.springframework.resilience.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientException;
 
@@ -21,40 +22,66 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
 public class ResilientProductCatalogAPIClient {
 
     private final ProductCatalogAPIClient productCatalogAPIClient;
+    private final CircuitBreaker circuitBreaker;
+
+    public ResilientProductCatalogAPIClient(CircuitBreakerFactory circuitBreakerFactory,
+                                            ProductCatalogAPIClient productCatalogAPIClient) {
+        this.productCatalogAPIClient = productCatalogAPIClient;
+        this.circuitBreaker = circuitBreakerFactory.create("productCatalogCB");
+    }
 
     @Cacheable(cacheNames = "algashop:product-catalog-api:v1", key = "#productId")
     @ConcurrencyLimit(10)
-    @Retryable(
-            maxRetries = 3,
-            delayString = "3s",
-            multiplier = 2,
-            includes = {GatewayTimeoutException.class, BadGatewayException.class}
-    )
     public Optional<ProductResponse> getById(UUID productId) {
+        log.info("Trying to load product {}", productId);
+        try {
+            return circuitBreaker.run(()->loadProduct(productId));
+        } catch (NoFallbackAvailableException e) {
+            if (e.getCause() instanceof RetryException re) {
+                if (re.getCause() instanceof GatewayTimeoutException gte) {
+                    throw gte;
+                }
+                if (re.getCause() instanceof BadGatewayException bge) {
+                    throw bge;
+                }
+            }
+            throw e;
+        }
+    }
+
+    private Optional<ProductResponse> loadProduct(UUID productId) {
         log.info("Loading product {}", productId);
         try {
             return Optional.ofNullable(productCatalogAPIClient.getById(productId));
-        } catch (HttpClientErrorException.NotFound e) {
+        } catch (HttpClientErrorException e) {
+            if (!(e instanceof HttpClientErrorException.NotFound)) {
+                log.error("Client HTTP error when loading product {}", productId, e);
+            }
             return Optional.empty();
         } catch (RestClientException e) {
             throw translateException(e);
         }
     }
 
-    private Exception translateException(RestClientException e) {
-        if (e.getCause() instanceof SocketTimeoutException) {
+    private RuntimeException translateException(RestClientException e) {
+        if (e.getCause() instanceof SocketTimeoutException
+            || e instanceof ResourceAccessException) {
             return new GatewayTimeoutException("Product Catalog API Timeout", e);
         }
+
         if (e instanceof HttpClientErrorException) {
-            return new ClientErrorException("Product Catalog API Client Error", e);
+            return new BadGatewayException.ClientErrorException("Product Catalog API Bad Gateway", e);
         }
+
+        if (e instanceof HttpServerErrorException) {
+            return new BadGatewayException.ServerErrorException("Product Catalog API Bad Gateway", e);
+        }
+
         return new BadGatewayException("Product Catalog API Bad Gateway", e);
     }
-
 
 }
