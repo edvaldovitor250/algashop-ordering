7b49f8c942df2a9c4fd89b952708e337fb6c709e
Edvaldo Vitor <edvaldovitor250@gmail.com>
2026-06-20 08:48:27 -0300

    Visualizando estado do Circuit Breaker

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/web/product/client/http/ResilientProductCatalogAPIClient.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/web/product/client/http/ResilientProductCatalogAPIClient.java
@@ -5,6 +5,9 @@ import com.algaworks.algashop.ordering.infrastructure.adapters.in.web.exceptionh
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cloud.circuitbreaker.retry.FrameworkRetryCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.retry.FrameworkRetryConfig;
+import org.springframework.cloud.circuitbreaker.retry.FrameworkRetryConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.cloud.client.circuitbreaker.NoFallbackAvailableException;
@@ -26,31 +29,38 @@ import java.util.UUID;
 public class ResilientProductCatalogAPIClient {
 
     private final ProductCatalogAPIClient productCatalogAPIClient;
-    private final CircuitBreaker circuitBreaker;
+    private final FrameworkRetryCircuitBreaker circuitBreaker;
 
-    public ResilientProductCatalogAPIClient(CircuitBreakerFactory circuitBreakerFactory,
+    public ResilientProductCatalogAPIClient(CircuitBreakerFactory<FrameworkRetryConfig,
+                                                    FrameworkRetryConfigBuilder> circuitBreakerFactory,
                                             ProductCatalogAPIClient productCatalogAPIClient) {
         this.productCatalogAPIClient = productCatalogAPIClient;
-        this.circuitBreaker = circuitBreakerFactory.create("productCatalogCB");
+        this.circuitBreaker = (FrameworkRetryCircuitBreaker) circuitBreakerFactory.create("productCatalogCB");
     }
 
     @Cacheable(cacheNames = "algashop:product-catalog-api:v1", key = "#productId")
     @ConcurrencyLimit(10)
     public Optional<ProductResponse> getById(UUID productId) {
         log.info("Trying to load product {}", productId);
+        log.info("Product catalog API CB state is {}", circuitBreaker.getCircuitBreakerPolicy().getState());
+
         try {
             return circuitBreaker.run(()->loadProduct(productId));
         } catch (NoFallbackAvailableException e) {
-            if (e.getCause() instanceof RetryException re) {
-                if (re.getCause() instanceof GatewayTimeoutException gte) {
-                    throw gte;
-                }
-                if (re.getCause() instanceof BadGatewayException bge) {
-                    throw bge;
-                }
-            }
-            throw e;
+            throw unwrapException(e);
         }
+    }
+
+    private RuntimeException unwrapException(NoFallbackAvailableException e) {
+        if (e.getCause() instanceof RetryException re) {
+            if (re.getCause() instanceof GatewayTimeoutException gte) {
+                return gte;
+            }
+            if (re.getCause() instanceof BadGatewayException bge) {
+                return bge;
+            }
+        }
+        return e;
     }
 
     private Optional<ProductResponse> loadProduct(UUID productId) {
