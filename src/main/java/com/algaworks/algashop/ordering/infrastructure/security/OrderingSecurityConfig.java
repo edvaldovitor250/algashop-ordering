68e9383e07fbc0c3715a227749e082f6a10c82f3
Edvaldo Vitor <edvaldovitor250@gmail.com>
2026-06-27 18:14:32 -0300

    Transformando Ordering em um OAuth2 Client

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/security/OrderingSecurityConfig.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/security/OrderingSecurityConfig.java
@@ -25,6 +25,7 @@ public class OrderingSecurityConfig {
                 .anyRequest().authenticated()
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+            .oauth2Client(Customizer.withDefaults());
 
         return http.build();
     }
