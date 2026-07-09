a9bd7fcdfd701bd2c9c3b31d16b152827c6c7e05
Edvaldo Vitor <edvaldovitor250@gmail.com>
2026-07-09 20:48:58 -0300

    Testes envolvendo API Client autenticadas

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/product/client/http/ProductCatalogAPIConfig.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/product/client/http/ProductCatalogAPIConfig.java
@@ -1,17 +1,24 @@
 package com.algaworks.algashop.ordering.infrastructure.adapters.out.web.product.client.http;
 
+import jakarta.validation.constraints.NotBlank;
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
+import org.springframework.security.oauth2.client.web.client.RequestAttributePrincipalResolver;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.time.Duration;
+import java.util.Collections;
 
 @Configuration
 public class ProductCatalogAPIConfig {
@@ -19,12 +26,7 @@ public class ProductCatalogAPIConfig {
     @Bean
     public ProductCatalogAPIClient productCatalogAPIClient(RestClient.Builder builder,
            ProductCatalogIntegrationProperties properties,
-           OAuth2AuthorizedClientManager manager) {
-
-        var interceptor = new OAuth2ClientHttpRequestInterceptor(manager);
-        interceptor.setClientRegistrationIdResolver(_ -> properties.getOauth2ClientRegistrationId());
-        interceptor.setAuthorizationRequestResolver(request -> { generatePrincipal.getOauth2AuthorizedClientManager() });    
-        
+           @Qualifier("productCatalogAPIClientInterceptor") OAuth2ClientHttpRequestInterceptor interceptor) {
 
         RestClient restClient = builder.baseUrl(properties.getUrl())
                 .requestFactory(generateClientHttpRequestFactory())
@@ -36,26 +38,35 @@ public class ProductCatalogAPIConfig {
         return proxyFactory.createClient(ProductCatalogAPIClient.class);
     }
 
+    @Bean("productCatalogAPIClientInterceptor")
+    public OAuth2ClientHttpRequestInterceptor productCatalogAPIClientInterceptor(
+            ProductCatalogIntegrationProperties properties,
+            OAuth2AuthorizedClientManager manager) {
+        var interceptor = new OAuth2ClientHttpRequestInterceptor(manager);
+        interceptor.setClientRegistrationIdResolver(_ -> properties.getOauth2ClientRegistrationId());
+        interceptor.setPrincipalResolver(_ -> generatePrincipal(properties.getOauth2ClientRegistrationId()));
+        return interceptor;
+    }
+
+    private Authentication generatePrincipal(String principalName) {
+        return new AbstractAuthenticationToken(Collections.emptySet()) {
+            @Override
+            public @Nullable Object getPrincipal() {
+                return principalName;
+            }
+
+            @Override
+            public @Nullable Object getCredentials() {
+                return null;
+            }
+        };
+    }
+
     private ClientHttpRequestFactory generateClientHttpRequestFactory() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setReadTimeout(Duration.ofSeconds(5));
         factory.setConnectTimeout(Duration.ofSeconds(2));
         return factory;
-    }
-
-    private Authentication generatePrincipal( String principalName) {
-        return new AbstractAuthenticationToken(Collections.emptyList()) {
-            @Override
-            public Object getCredentials() {
-                return null;
-            }
-
-            @Override
-            public Object getPrincipal() {
-                return principalName;
-            }
-        };
-           
     }
 
 }
--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/AbstractPersistenceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/AbstractPersistenceIT.java
@@ -1,16 +1,31 @@
-package com.algaworks.algashop.ordering.infrastructure.persistence;
+package com.algaworks.algashop.ordering.infrastructure.adapters.out.persistence;
 
-import com.algaworks.algashop.ordering.infrastructure.persistence.TestcontainerPostgreSQLConfig;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.algaworks.algashop.ordering.core.application.security.SecurityChecks;
+import com.algaworks.algashop.ordering.infrastructure.config.auditing.SpringDataAuditingConfig;
+import com.algaworks.algashop.ordering.utils.TestcontainerPostgreSQLConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.UUID;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import({TestcontainerPostgreSQLConfig.class, SpringDataAuditingConfig.class})
 public abstract class AbstractPersistenceIT {
 
-}
+	@MockitoBean
+	protected SecurityChecks securityChecks;
 
+	@BeforeEach
+	public void setup() {
+		Mockito.when(securityChecks.isAuthenticated()).thenReturn(true);
+		Mockito.when(securityChecks.isMachineAuthenticated()).thenReturn(false);
+		Mockito.when(securityChecks.getAuthenticatedUserId()).thenReturn(UUID.randomUUID());
+	}
+
+}
